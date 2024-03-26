24a800b29c4eb9a23a20a07d6cb82b6033c78aae
Amr-Azab <20194736@fue.edu.eg>
2024-03-26 04:10:31 +0200

    update code

--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
-process.on("uncaughtException", (err) => {
-  console.log("UNCAUTCHT EXCEPTION ! Shutting down...");
-  console.log(err.name, err.message);
-  process.exit(1);
-});
-dotenv.config({ path: "./config.env" });
+// process.on("uncaughtException", (err) => {
+//   console.log("UNCAUTCHT EXCEPTION ! Shutting down...");
+//   console.log(err.name, err.message);
+//   process.exit(1);
+// });
+
 const app = require("./app");
 
 const port = 8000;
-
+dotenv.config({ path: "./config.env" });
 const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
   process.env.DATABASE_PASSWORD
@@ -28,10 +28,10 @@ app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
-process.on("unhandledRejection", (err) => {
-  console.log("UNHANDLED REJECTION ! 💥 Shutting down...");
-  console.log(err.name, err.message);
-  server.close(() => {
-    process.exit(1);
-  });
-});
+// process.on("unhandledRejection", (err) => {
+//   console.log("UNHANDLED REJECTION ! 💥 Shutting down...");
+//   console.log(err.name, err.message);
+//   server.close(() => {
+//     process.exit(1);
+//   });
+// });
